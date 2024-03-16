832a10af6cad17ac08d522d1d1173de341f2739f
KlasniMisho123 <117183990+KlasniMisho123@users.noreply.github.com>
2024-03-17 00:54:35 +0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".dotalogo").on("click", function() {
+        alert("Logo clicked!");
+        // You can add more actions here
+    });
+});
+
+console.log("functions.js is executed!");
--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const express = require('express');
-const path = require('path');
+import bodyParser from "body-parser";
+import express from "express";
+import axios from "axios";
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/", async (req, res) => { 
+  try {
+    let heroName = req.body.heroName
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    const response = await axios.get(`https://api.opendota.com/api/heroStats`);
+
+    const heroStats = response.data
+  
+    heroStats.forEach(hero => {
+      
+    });
+
+    // "localized_name": "Phantom Assassin",
+    // chawerili saxelis mixedvit gmiris povna da mxolod misi statebis naxva
+
+    heroName = heroName.replace(" ", "_")
+
+    
+    
+    console.log(heroName);
+    res.render("index.ejs", {selectedHero : heroName});
+  } catch {
+    console.log("errors");
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
