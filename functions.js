838de72bdfd1443b10ecd72b78026d868fd788b1
KlasniMisho123 <117183990+KlasniMisho123@users.noreply.github.com>
2024-03-18 21:05:03 +0400

    Delete functions.js

--- functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".dotalogo").on("click", function() {
-        alert("Logo clicked!");
-        // You can add more actions here
-    });
-});
-
-console.log("functions.js is executed!");
